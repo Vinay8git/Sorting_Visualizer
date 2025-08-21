aeffc9835ebbf6eb7c6f3e3e66784ca17df6a83f
Vinay8git <vinayyd277@gmail.com>
2025-08-21 11:17:43 +0530

    Final_Commit

---
+++ src/Visualizer.jsx
@@ -0,0 +1,208 @@
+import React, { Component } from 'react';
+import TextLoop from "react-text-loop";
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import './Visualizer.css'
+
+export default class Visualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 'main',
+            rendering: false,
+            algorithms: [],
+            currentAlgorithm: null,
+            goFunction: () => { },
+            resetFunction: () => { },
+            setAlgorithm: () => { },
+            sortingClicked: false,
+            pathClicked: false,
+            AIClicked: false,
+            aicount: 0,
+        };
+        this.getFunctions = this.getFunctions.bind(this);
+        this.changeRenderingState = this.changeRenderingState.bind(this);
+    }
+
+    changeRenderingState(rendering) {
+        this.setState({ rendering: rendering });
+    }
+
+    getFunctions(go, reset, setAlgo, algorithms) {
+        this.state.goFunction = go;
+        this.state.resetFunction = reset;
+        this.state.setAlgorithm = setAlgo;
+        this.state.algorithms = algorithms;
+        this.setState({ algorithms: algorithms });
+    }
+
+    render() {
+        let renderObj = null;
+        if (this.state.mode === 'sorting') {
+            renderObj = <SortingVisualizer setVisualizerRendering={this.changeRenderingState} getFunctions={this.getFunctions} />;
+        }
+        else {
+            renderObj =
+                <div class="welbotron">
+
+                    <div class="container welc">
+
+                        <h1 class='welcome'>Hello, algorithms.
+                            <p class="quote">
+                                <TextLoop interval={3800} springConfig={{ stiffness: 200 }} adjustingSpeed={300} >
+                                    <p class="quoteText">"An algorithm must be seen to be believed."</p>
+                                    <p class="quoteText">"Algorithms are central objects of study in Computer Science."</p>
+                                    <p class="quoteText">"Algorithms are apprehensible magics."</p>
+                                    <p class="quoteText">"An algorithm is like a recipe."</p>
+                                </TextLoop>
+                            </p>
+
+                            <p class="lead">This website might help you understand algorithms better by visualizing them.</p>
+                            <p class="secondline lead">Click on one of the categories below to visualize algorithms.</p>
+
+                        </h1>
+                        <a href='#' class='mainpage-b' onClick={() => {
+                            if (!this.state.rendering) {
+                                this.setState({ mode: 'pathfinding' });
+                                this.setState({ currentAlgorithm: null, pathClicked: true });
+                            }
+                        }} data-toggle={this.state.pathClicked ? "" : "modal"} data-target="#pathIntroModal">
+                            <span></span>
+                            PATH FINDING
+                        </a>
+                        <a href='#' class='mainpage-b' onClick={() => {
+                            if (!this.state.rendering) {
+                                this.setState({ mode: 'sorting', currentAlgorithm: null, sortingClicked: true });
+                            }
+                        }} data-toggle={this.state.sortingClicked ? "" : "modal"} data-target="#sortingIntroModal">
+                            <span></span>
+                            SORTING
+                        </a>
+                        <a href='#' class='mainpage-b' onClick={() => {
+                            if (!this.state.rendering) {
+                                this.setState({ mode: 'ai', currentAlgorithm: null, AIClicked: true });
+                            }
+                        }} data-toggle={this.state.AIClicked ? "" : "modal"} data-target="#aiIntroModal">
+                            <span></span>
+                            ARTIFICIAL INTELLIGENCE
+                        </a>
+                    </div>
+                </div>
+        }
+        let invisibleOrNot = '';
+        if (this.state.mode === 'main') invisibleOrNot = ' invisible';
+        let algorithms = this.state.algorithms;
+        return (
+            <>
+                <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-dark">
+                    <button
+                        onClick={() => {
+                            if (!this.state.rendering) {
+                                this.setState({ mode: 'main' });
+                            }
+                        }}
+                        type="button" class="btn btn-dark navbtn"
+                        disabled={this.state.rendering}
+                    >Main</button>
+
+                    
+
+                    <button
+                        onClick={() => {
+                            if (!this.state.rendering) {
+                                this.setState({ mode: 'sorting', currentAlgorithm: null, sortingClicked: true });
+                                this.state.setAlgorithm(-1);
+                            }
+                        }}
+                        type="button" class="btn btn-dark navbtn"
+                        data-toggle={this.state.sortingClicked ? "" : "modal"} data-target="#sortingIntroModal"
+                        disabled={this.state.rendering}
+                    >Sorting</button>
+
+                    <div class={"dropdown" + invisibleOrNot}>
+                        <button class="btn btn-secondary dropdown-toggle navbtn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" disabled={this.state.rendering}>
+                            {this.state.currentAlgorithm == null ? 'Algorithms' : this.state.currentAlgorithm}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li>
+                                {algorithms.map((algorithm, algoId) => {
+                                    return (<button type="button" class="btn btn-light navbtn" onClick={() => {
+                                        this.state.setAlgorithm(algoId);
+                                        this.setState({ currentAlgorithm: this.state.algorithms[algoId] });
+                                    }}>{algorithm}</button>);
+                                }
+                                )
+                                }
+                            </li>
+                        </div>
+                    </div>
+
+                    <div class={"dropdown" + invisibleOrNot}>
+                        <button class="btn btn-light dropdown-toggle navbtn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" disabled={this.state.rendering}>
+                            Actions
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li>
+                                <button type="button" class="btn btn-light navbtn" onClick={() => this.state.goFunction()} data-toggle={this.state.currentAlgorithm === null ? "modal" : ""} data-target="#setAlgoModal" disabled={this.state.mode === "ai" && this.state.currentAlgorithm === "Minimax"}>Go!</button>
+                                <button type="button" class="btn btn-light navbtn" onClick={() => this.state.resetFunction()}>Reset</button>
+                            </li>
+                        </div>
+                    </div>
+                </nav>
+
+                <div class="modal fade" id="setAlgoModal" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+
+                            <div class="modal-header">
+                                <h5 class="modal-title">No Algorithm Selected</h5>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <div class="modal-body-alert">
+                                <p>Please select an algorithm first.</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-dark" data-dismiss="modal" style={{ width: '100px' }}>OK</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div class="modal fade" id="sortingIntroModal" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content intro">
+
+                            <div class="modal-header">
+                                <h5 class="modal-title">Sorting</h5>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <div class="modal-body intro">
+                                <p>Sorting is a process of arranging an ordered sequence. It is a common operation in many applications.</p>
+                                <p>Common uses of sorted sequences are:
+                                    <div class='uses-list'>
+                                        <p>·lookup or search efficiently</p>
+                                        <p>·merge sequences efficiently</p>
+                                        <p>·process data in a defined order</p>
+                                    </div>
+                                Now please choose a sorting algorithm and visualize it!
+                                </p>
+                                <p class='tips'>(after choosing an algorithm, click on the [Actions] button.)</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-dark" data-dismiss="modal" style={{ width: '100px' }}>OK</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div>
+                    {renderObj}
+                </div>
+            </>
+        )
+    }
+}
